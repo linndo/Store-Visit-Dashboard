cea32f7aebbe9b4ac5eade7d1ec717814a5d341e
Linda Pöhlmann <poehlmannlinda@gmail.com>
2026-04-01 15:57:35 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header">
+    <h1 class="title">Store Visit Dashboard</h1>
+  </header>
+  <main class="main">
+    <div>
+      *Placeholder*
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.header {
+  background-color: #103166;
+  color: white;
+  padding: 2rem;
+  margin: 2rem auto;
+  width: 60%;
+}
+
+.main {
+  margin: 2rem auto;
+  width: 60%;
+}
+
+.title{
+  margin: 0;
+  font-size: 1.5rem;
+}
+
+body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
